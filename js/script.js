0226c6da06e76e16a82d82c9dfd67fb4db0a70af
alexeyyago <47055611+alexeyyago@users.noreply.github.com>
2020-10-14 12:11:27 +0300

    Атрибуты required для input

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 let searchForm = document.querySelector('.search-form');
 let searchFormOpenButton = document.querySelector('.search-form-open-button');
 let searchFormDateIn = searchForm.querySelector('[name="date-in"]');
-let searchFormDateOut = searchForm.querySelector('[name="date-out"]');
 let searchFormNumberAdults = searchForm.querySelector('[name="number-adults"]');
 let searchFormNumberKids = searchForm.querySelector('[name="number-kids"]');
 
@@ -14,5 +13,21 @@ searchFormOpenButton.addEventListener('click', function(evt) {
   evt.preventDefault();
   searchForm.classList.toggle('search-form--hide');
 
-  // searchFormDateIn.focus();
+  searchFormDateIn.focus();
+});
+
+try {
+  storage = localStorage.getItem("number-adults");
+  console.log('норм');
+} catch (err) {
+  isStorageSupport = false;
+}
+
+searchForm.addEventListener('submit', function(evt) {
+  if (searchFormNumberAdults.value || searchFormNumberKids.value) {
+    if (isStorageSupport) {
+      localStorage.setItem("number-adults", searchFormNumberAdults.value);
+      localStorage.setItem("number-kids", searchFormNumberKids.value);
+    }
+  }
 });
