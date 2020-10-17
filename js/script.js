9b27d920a2f081e70c30c0dbfd2aa7d1aa8399e5
alexeyyago <47055611+alexeyyago@users.noreply.github.com>
2020-10-17 17:11:15 +0300

    валидация формы

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let searchForm = document.querySelector('.search-form');
 let searchFormOpenButton = document.querySelector('.search-form-open-button');
 let searchFormDateIn = searchForm.querySelector('[name="date-in"]');
+let searchFormDateOut = searchForm.querySelector('[name="date-out"]');
 let searchFormNumberAdults = searchForm.querySelector('[name="number-adults"]');
 let searchFormNumberKids = searchForm.querySelector('[name="number-kids"]');
 
@@ -12,14 +13,22 @@ searchFormOpenButton.addEventListener('click', function(evt) {
   searchFormDateIn.focus();
 });
 
+const isStorageSupport = true;
+const storage = '';
+
 try {
   storage = localStorage.getItem('number-adults');
 } catch (err) {
   isStorageSupport = false;
 }
 
-searchForm.addEventListener('submit', function() {
-  if (searchFormNumberAdults.value || searchFormNumberKids.value) {
+searchForm.addEventListener('submit', function(evt) {
+  if (!searchFormDateIn.value || !searchFormDateOut.value || !searchFormNumberAdults.value || !searchFormNumberKids.value) {
+    evt.preventDefault();
+    searchForm.classList.remove('search-form--error');
+    searchForm.offsetWidth = searchForm.offsetWidth;
+    searchForm.classList.add('search-form--error');
+  } else {
     if (isStorageSupport) {
       localStorage.setItem('number-adults', searchFormNumberAdults.value);
       localStorage.setItem('number-kids', searchFormNumberKids.value);
